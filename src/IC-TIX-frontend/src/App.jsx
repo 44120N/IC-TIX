1fef16ff91d70aedd4a698cebb50b387c94637e6
Benedict02 <benedicthalim1@gmail.com>
2025-03-03 15:20:44 +0700

    janlup npm install. Frontend Changes: - add new icon - Navbar - Homepage - Login gtw bs ato gk

--- src/IC-TIX-frontend/src/App.jsx
+++ src/IC-TIX-frontend/src/App.jsx
@@ -1,30 +1,14 @@
-import { useState } from 'react';
-import { IC_TIX_backend } from 'declarations/IC-TIX-backend';
+
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    IC_TIX_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" Component="Home"/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/IC-TIX-frontend/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+
+import { Container, Stack, Typography } from "@mui/material";
+
+export default function Home(){
+    return(
+        <>
+            <Container>
+                <Stack>
+                    <Typography variant="h1">
+                    This is the Home Page
+                    </Typography>
+                </Stack>
+            </Container>
+        </>
+    );
+}
